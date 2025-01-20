c7aee25d78268dffd0ce063aa1757b7a56c99d59
Ivan Sierra <ivansierra259@gmail.com>
2025-01-20 18:54:33 -0500

    First commit

---
+++ contenido HTML/sitio_estructura/dist/app.js
@@ -0,0 +1,9 @@
+'use strict'; //modo estricto en JS
+
+//variables -> contenedores
+var contenedor1 = 19; //global. no usar
+let contenedor2 = 23; //local
+const cont3 = 10; //constante
+
+alert("Hola estudiantes"); 
+console.log(contenedor1*contenedor2+cont3); 
